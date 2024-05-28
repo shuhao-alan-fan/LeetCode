546d8ffc76b31b9767f5bff4177f619351eb9ebd
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-05-28 06:03:41 -0400

    Time: 13 ms (77.52%), Space: 17 MB (43.84%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -11,39 +11,35 @@
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode * dummy_head = nullptr;
-        for(int i = 0; i<lists.size(); i++){
-            dummy_head = mergeTwoLists(dummy_head,lists[i]);
-            cout<<i<<endl;
-        }
-        return dummy_head;
+        // ListNode * dummy_head = nullptr;
+        // while(list.size() > 1){
+        //     for(int i = 0; i<lists.size(); i++){
+        //         dummy_head = mergeTwoLists(dummy_head,lists[i]);
+        //     }
+        // }
+        
+        // return dummy_head;
+        int n = lists.size();
+        if(n == 0) return NULL;
+        return partionLists(0, n - 1, lists);
     }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       
-        ListNode * head = new ListNode(0);
-        ListNode * cur = head;
-        
-        while(list1 && list2){
-            
-            if(list1->val<list2->val){
-                cur->next = list1;
-                list1 = list1->next;
-
-            }
-            else{
-                cur->next = list2;
-                list2 = list2->next;
-            }
-
-            cur = cur->next;
-    
+        if (list1 == nullptr) return list2;
+        if (list2 == nullptr) return list1;
+        if (list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
         }
-        if(list1 == nullptr){
-            cur->next = list2;
+        else{
+             list2->next = mergeTwoLists(list1,list2->next);
+             return list2;
         }
-        if(list2 == nullptr){
-            cur->next = list1;
-        }
-        return head->next;
+    }
+    ListNode * partionLists(int lo, int hi, vector<ListNode*>& lists){
+        if(hi == lo) return lists[hi];
+        int mid = lo + (hi - lo) / 2;
+        ListNode *l1 = partionLists(lo,mid,lists);
+        ListNode *l2 = partionLists(mid+1,hi,lists);
+        return mergeTwoLists(l1,l2);
     }
 };
