24cddcfb7380d940645bedbe2b4c8370c2cc5392
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-06-18 16:13:33 +0800

    Time: 67 ms (81.65%), Space: 17 MB (74.87%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,13 +1,19 @@
 class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        res = max(nums)
-        maxp, minp = 1,1
-        for n in nums:
-            if n == 0: 
-                maxp, minp = 1,1
-                continue
-            temp = maxp * n
-            maxp = max(n*maxp, n*minp, n)
-            minp = min(temp,n*minp, n)
-            res = max(res,maxp)
-        return res
+    def maxProduct(self, nums: List[int]):
+        #the difference is to do kaDane from left to right and right to left
+        max_end_here = 1
+        max_so_far = -math.inf
+        for i in nums:
+            max_end_here *= i
+            if max_so_far < max_end_here:
+                max_so_far = max_end_here
+            if max_end_here == 0:
+                max_end_here = 1
+        max_end_here = 1
+        for i in reversed(nums):
+            max_end_here *= i
+            if max_so_far < max_end_here:
+                max_so_far = max_end_here
+            if max_end_here == 0:
+                max_end_here = 1
+        return max_so_far
