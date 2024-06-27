8620b7398e4f6d92b6df226f8bf05386831d1edb
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-06-27 16:17:31 +0800

    Time: 1007 ms (50.11%), Space: 35.6 MB (45.01%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
-        dp = {}
-        def dfs(i, even):
-            if (i == len(nums)):
-                return 0
-            total = nums[i] if even else -1 * nums[i]
-            if((i,even) in dp):
-                return dp[(i,even)]
-            dp[(i,even)] = max(total + dfs(i+1, not even), dfs(i+1, even))
-            return dp[(i,even)]
-        return dfs(0,True)
+        n = len(nums)
+        dp = [[0,0] for _ in range(n)]
+        dp[0][0] = nums[0]
+        dp[0][1] = 0
+
+       
+        for i in range(n):
+            dp[i][0] = max(dp[i-1][1] + nums[i], dp[i-1][0])
+            dp[i][1] = max(dp[i-1][0] - nums[i], dp[i-1][1])
+
+
+        return max(dp[n-1])
