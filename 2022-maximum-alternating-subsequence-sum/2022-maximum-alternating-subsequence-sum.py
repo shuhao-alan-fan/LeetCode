5f7bdad9feb9a464d57a805856b771a9f220a3ab
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-06-27 16:09:50 +0800

    Time: 1825 ms (5.1%), Space: 311.5 MB (24.42%) - LeetHub

---
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        dp = {}
+        def dfs(i, even):
+            if (i == len(nums)):
+                return 0
+            total = nums[i] if even else -1 * nums[i]
+            if((i,even) in dp):
+                return dp[(i,even)]
+            dp[(i,even)] = max(total + dfs(i+1, not even), dfs(i+1, even))
+            return dp[(i,even)]
+        return dfs(0,True)
