cccd6813afad4845db897118319bc319e53fc233
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-07-23 23:47:26 +0800

    Time: 1014 ms (22.53%), Space: 12.1 MB (11.73%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -9,8 +9,7 @@ public:
         s = word.size();
         for(int i = 0; i<row; i++){
             for(int j = 0; j< col; j++){
-                vector<pair<int,int>> v = {};
-                if(dfs(i,j,0,v,word,board)){
+                if(dfs(i,j,0,word,board)){
                     return true;
                 }
             }
@@ -18,23 +17,24 @@ public:
         return false;
     }
 
-    bool dfs(int i, int j, int idx, vector<pair<int,int>>& v,string word,vector<vector<char>>& board){
+    bool dfs(int i, int j, int idx,string word,vector<vector<char>>& board){
         if(i< 0 || i>= row || j>= col || j < 0 || idx >= s || board[i][j] != word[idx]){
             return false;
         }
         // cout<<board[i][j]<<endl;
         // if(idx == s-1 && board[i][j] == word[idx]) return true;
         
-        for(auto iterator :v){
-            if(iterator.first == i && iterator.second == j) return false;
-        }
+        // for(auto iterator :v){
+        //     if(iterator.first == i && iterator.second == j) return false;
+        // }
         if(idx == s-1 && board[i][j] == word[idx]) return true;
-        v.emplace_back(i,j);
+        char c = board[i][j];
 
-        bool res = dfs(i-1,j,idx+1,v,word,board) || dfs(i+1,j,idx+1,v,word,board) 
-        || dfs(i,j-1,idx+1,v,word,board) || dfs(i,j+1,idx+1,v,word,board);
+        board[i][j] = '*';
+        bool res = dfs(i-1,j,idx+1,word,board) || dfs(i+1,j,idx+1,word,board) 
+        || dfs(i,j-1,idx+1,word,board) || dfs(i,j+1,idx+1,word,board);
 
-        v.pop_back();
+        board[i][j] = c;
         return res;
 
     }
