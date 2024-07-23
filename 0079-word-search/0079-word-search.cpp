c70db7ba23ed3730a52f48be9b516b92496de0d0
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-07-23 23:50:05 +0800

    Time: 1008 ms (24.8%), Space: 12.1 MB (11.73%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    int row;
-    int col;
-    int s;
+    
     bool exist(vector<vector<char>>& board, string word) {
-        row = board.size();
-        col = board[0].size();
-        s = word.size();
+        int row = board.size();
+        int col = board[0].size();
         for(int i = 0; i<row; i++){
             for(int j = 0; j< col; j++){
                 if(dfs(i,j,0,word,board)){
@@ -18,15 +15,12 @@ public:
     }
 
     bool dfs(int i, int j, int idx,string word,vector<vector<char>>& board){
+        int row = board.size();
+        int col = board[0].size();
+        int s = word.size();
         if(i< 0 || i>= row || j>= col || j < 0 || idx >= s || board[i][j] != word[idx]){
             return false;
         }
-        // cout<<board[i][j]<<endl;
-        // if(idx == s-1 && board[i][j] == word[idx]) return true;
-        
-        // for(auto iterator :v){
-        //     if(iterator.first == i && iterator.second == j) return false;
-        // }
         if(idx == s-1 && board[i][j] == word[idx]) return true;
         char c = board[i][j];
 
