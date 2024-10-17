04bd37705b1f1e9953943d977793268f43e0f7ab
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-10-17 16:40:37 -0400

    Time: 2324 ms (7.01%), Space: 16.6 MB (97.5%) - LeetHub

--- 0212-word-search-ii/0212-word-search-ii.cpp
+++ 0212-word-search-ii/0212-word-search-ii.cpp
@@ -1,16 +1,14 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <unordered_set>
-#include <stack>
-using namespace std;
-
 struct Node {
     char val;
-    unordered_map<char, Node*> children;
+    vector<Node*> children;
+    Node* parent;
     bool terminal = false;
-    Node(char value) : val(value) {}
+    Node(char value){
+        val = value;
+    }
 };
+
+
 
 class Solution {
 public:
@@ -20,66 +18,79 @@ public:
         vector<string> sol;
         res.clear();
         build_tree(words);
-        int row = board.size(), col = board[0].size();
-        vector<vector<bool>> visited(row, vector<bool>(col, false));
-        for(int i = 0; i < row; i++) {
-            for(int j = 0; j < col; j++) {
-                if (root->children.find(board[i][j]) != root->children.end()) {
-                    dfs(i, j, board, root, "", visited);
-                }
+        Node* cur = root;
+        for(int i = 0; i<board.size(); i ++){
+            for(int j = 0; j<board[i].size(); j++){
+
+                dfs(i,j,board, cur, "");
+            
             }
         }
-        for(const string& word : res) {
-            sol.push_back(word);
+        for(string i : res){
+            sol.push_back(i);
         }
         return sol;
     }
 
-private:
-    void dfs(int i, int j, vector<vector<char>>& board, Node* cur, string word, vector<vector<bool>>& visited) {
+    void dfs(int i, int j, vector<vector<char>>& board, Node* & p, string word){
         int row = board.size(), col = board[0].size();
-        stack<tuple<int, int, Node*, string>> stk;
-        stk.push({i, j, cur, word});
-
-        while (!stk.empty()) {
-            auto [x, y, node, currentWord] = stk.top();
-            stk.pop();
-
-            if (x < 0 || y < 0 || x >= row || y >= col || visited[x][y] || node == nullptr) {
-                continue;
-            }
-
-            char c = board[x][y];
-            if (node->children.find(c) == node->children.end()) {
-                continue;
-            }
-
-            visited[x][y] = true;
-            node = node->children[c];
-            currentWord += c;
-            if (node->terminal) {
-                res.insert(currentWord);
-            }
-
-            stk.push({x-1, y, node, currentWord});
-            stk.push({x+1, y, node, currentWord});
-            stk.push({x, y-1, node, currentWord});
-            stk.push({x, y+1, node, currentWord});
-
-            visited[x][y] = false;
+        Node* cur = p;
+        if(i < 0 || j < 0 || i >= row || j >= col || cur == nullptr){
+            return ;
         }
-    }
-
-    void build_tree(const vector<string>& words) {
-        for(const string& word : words) {
-            Node* cur = root;
-            for(char c : word) {
-                if(cur->children.find(c) == cur->children.end()) {
-                    cur->children[c] = new Node(c);
+        bool found = false;
+        for(Node* child : cur->children){
+            if(board[i][j] == child->val){
+                cur = child;
+                word = word + child->val;
+                found = true;
+                if(child->terminal){
+                    res.insert(word);
                 }
-                cur = cur->children[c];
+                
             }
-            cur->terminal = true;
         }
+        if(!found){
+            return;
+        }
+        char c = board[i][j];
+        board[i][j] = '*';
+        dfs(i-1,j,board,cur,word);
+        dfs(i+1,j,board,cur,word);
+        dfs(i,j-1,board,cur,word);
+        dfs(i,j+1,board,cur,word);
+        board[i][j] = c;
     }
+
+
+    void build_tree(vector<string>& words){
+        for(string s : words){
+            Node* cur = root;
+            for(int i = 0; i<s.size(); i++){
+                bool found = false;
+                for(Node* p : cur->children){
+                    if(p->val == s[i]){
+                        found = true;
+                        cur = p;
+                        break;
+                    }
+                }
+                if(!found){
+                    
+                    Node* n = new Node(s[i]);
+                    n-> parent = cur;
+                    cur->children.push_back(n);
+                    cur = n;
+                    
+                }
+                if(i == s.size()-1){
+                    cur->terminal = true;
+                }
+                
+            }
+        }
+        
+        return;
+    }
+    
 };
