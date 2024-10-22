9e47bf3d0579df49e02c17fe8aa0380ec96e6b23
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-10-22 14:41:55 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,18 +1,27 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class Solution {
 public:
-    unordered_map<int,Node*> map;
-    Node* cloneGraph(Node* node){
-        if(!node) return nullptr;
-        if(map.find(node->val) != map.end()) return map[node->val];
-
-        Node* copy_node = new Node(node->val);
-        map[node->val] = copy_node;
-        for(Node* n : node->neighbors){
-            
-            copy_node->neighbors.push_back(cloneGraph(n));
+    Node* cloneGraph(Node* node) {
         
-        }
-        return copy_node;
     }
-    
 };
