2a6c07977b856885b84b14faebfa9eb09d5a094f
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-11-01 13:57:48 -0400

    Time: 6 ms (27.66%), Space: 21.6 MB (62.92%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,40 +1,38 @@
 class MinStack {
 public:
-    struct minStack{
-        stack<int> s;
-        stack<int> min_s;
-    };
     
-    minStack* ms;
+    stack<int> s;
+    stack<int> min_s;
+    
+
     MinStack() {
-        this->ms = new minStack();
     }
     
     void push(int val) {
-        this->ms->s.push(val);
+       s.push(val);
         
-        if(this->ms->min_s.empty() || val <= this->ms->min_s.top()){
+        if(min_s.empty() || val <= min_s.top()){
             
-            this->ms->min_s.push(val);
+            min_s.push(val);
             
         }
     
     }
-    //fix this
+    
     void pop() {
-        int temp = this->ms->s.top();
-        if(!this->ms->min_s.empty() && this->ms->min_s.top() == temp){
-            this->ms->min_s.pop();
+
+        if(!min_s.empty() && min_s.top() == s.top()){
+            min_s.pop();
         }
-        this->ms->s.pop();
+        s.pop();
     }
     
     int top() {
-        return this->ms->s.top();
+        return s.top();
     }
     
     int getMin() {
-        return this->ms->min_s.top();
+        return min_s.top();
     }
 };
 
