10336aa3fa6beb119e0bbb53b7e7e7fb94dffaf5
Alan20030718 <75001847+Alan20030718@users.noreply.github.com>
2024-11-01 12:00:52 -0400

    Time: 32 ms (59.28%), Space: 137.2 MB (83.3%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,38 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> res;
-        int max_ind;
-        int l = 0, r = k - 1;
-        int m = INT_MIN;
-        for(int i = 0; i<k; i++){
+       deque<int> dq;
+       vector<int> res;
+       
+       
+       for(int i = 0; i<nums.size(); i++){
+            //get rid of front
+            if(!dq.empty() && dq.front() == i - k){
+                
+                dq.pop_front();
+            }
+
+            while(!dq.empty() && nums[dq.back()] < nums[i]){
+                dq.pop_back();
+            }
+            //push_back
+            dq.push_back(i);
+            if(i >= k - 1){
+                res.push_back(nums[dq.front()]);
+            }
             
-            if(nums[i] >= m){
-                m = nums[i];
-                max_ind = i;
-            }
-        }
-        res.push_back(m);
-        for(int i = k; i<nums.size(); i++){
-
-            if(max_ind == i - k){
-                m = nums[i - k + 1];
-
-                for(int j = i-k+1; j<i+1; j++){
-
-                    if(nums[j] >= m){
-                        m = nums[j];
-                        max_ind = j;
-                    }
-
-                }
-            }
-            if(nums[i] >= m){
-                m = nums[i];
-                max_ind = i;
-            }
-             res.push_back(m);
-        }
-        return res;
+       }
+       return res;
     }
 };
