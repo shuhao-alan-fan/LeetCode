0f9c71a8d95fa6fe379546844cd7f2c13e83b34a
Alan718 <75001847+Alan20030718@users.noreply.github.com>
2025-03-18 23:47:07 -0400

    Time: 2 ms (39.23%), Space: 10.6 MB (70.44%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        dfs(0,nums, ans);
+        return ans;
+    }
+    void dfs(int n, vector<int>& nums, vector<vector<int>>& ans){
+
+        // check termination condition & append the current change
+        
+        if(n >= nums.size()){
+            ans.push_back(nums);
+            return;
+        }
+        
+        //body of dfs, swap each two numbers for each n, all the way to the-
+        //-whole num array
+
+        for(int i = n; i<nums.size(); i++){
+            swap(nums[n], nums[i]);
+            dfs(n+1, nums, ans);
+            swap(nums[n], nums[i]);
+        }
+        return;
+    }
+};
