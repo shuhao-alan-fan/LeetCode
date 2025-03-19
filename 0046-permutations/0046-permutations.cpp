76368ad7bec609630712f3d78634a0f8b6edb8e1
Alan718 <75001847+Alan20030718@users.noreply.github.com>
2025-03-18 23:48:29 -0400

    Time: 0 ms (100%), Space: 10.4 MB (97.83%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+
+        //added this check to make the runtime faster
+
+        if (nums.size() == 0) {
+            return {};
+        }
+
+        //implementation:
+
         vector<vector<int>> ans;
         dfs(0,nums, ans);
         return ans;
@@ -8,7 +17,7 @@ public:
     void dfs(int n, vector<int>& nums, vector<vector<int>>& ans){
 
         // check termination condition & append the current change
-        
+
         if(n >= nums.size()){
             ans.push_back(nums);
             return;
