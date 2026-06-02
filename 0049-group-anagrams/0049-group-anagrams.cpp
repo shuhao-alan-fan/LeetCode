24f84d7db28d4550648aca5b5af48f3282725951
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-06-02 12:43:40 -0400

    Time: 20 ms (37.85%), Space: 25 MB (79.18%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // how to compare the chars between two strings
+        unordered_map<string,vector<string>> mp;
+        for(string s:strs){
+            string unsort = s;
+            sort(s.begin(),s.end());
+            if(mp.find(s) == mp.end()) mp[s].push_back(unsort);
+            else{
+                mp[s].push_back(unsort);
+            }
+        }
+        vector<vector<string>> ans;
+        for(const auto & [ana,list] : mp){
+            ans.push_back(list);
+        }
+        return ans;
+        
+    }
+};
