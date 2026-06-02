175ae5ecb745e1d61f5c64556d699ebf648e8794
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-06-02 13:07:38 -0400

    Time: 0 ms (100%), Space: 11.6 MB (89.07%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int count[26] = {0};
+        for(char c:ransomNote){
+            count[c - 'a']--;
+        }
+        
+        for(char c:magazine){
+            count[c - 'a']++;
+        }
+
+        for(int i = 0; i<26; i++){
+            if(count[i] < 0) return false;
+        }
+        return true;
+    }
+};
