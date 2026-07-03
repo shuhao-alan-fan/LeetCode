8295c8b84eca3809fc5a7a710f2e31002335edab
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-07-03 05:17:51 -0400

    Time: 9 ms (7.11%), Space: 9.7 MB (49.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int count1[26] = {0};
-        for(char c:s){
-            count1[c - 'a']++;
-        }
-        int count2[26] = {0};
-        for(char c:t){
-            count2[c - 'a']++;
-        }
-
-        string key1;
-        string key2;
-        for(int i = 0; i<26; i++){
-            key1 += to_string(count1[i]) + '#';
-            key2 += to_string(count2[i]) + '#';
-        }
-        return key1 == key2;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
