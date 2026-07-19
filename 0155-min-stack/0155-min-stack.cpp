6a1796ffc4a204c34d69a52f4c790f66f19527ff
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-07-19 11:00:23 +0800

    Time: 48 ms (68.79%), Space: 151 MB (74.2%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,45 +1,40 @@
+// O(1)怎么可能呢，所有都是O(1)的话没办法push和pop的同时留住min
+
 class MinStack {
+private: 
+    stack<pair<int,int>> s;
 public:
     
-    stack<int> s;
-    stack<int> min_s;
-    
-
     MinStack() {
+        
     }
     
-    void push(int val) {
-       s.push(val);
-        
-        if(min_s.empty() || val <= min_s.top()){
-            
-            min_s.push(val);
-            
+    void push(int value) {
+        if(s.empty()) s.push(make_pair(value, value));
+        else{
+            int min_val = min(value, s.top().second);
+            s.push(make_pair(value, min_val));
         }
-    
+
     }
     
     void pop() {
-
-        if(!min_s.empty() && min_s.top() == s.top()){
-            min_s.pop();
-        }
         s.pop();
     }
     
     int top() {
-        return s.top();
+        return s.top().first;
     }
     
     int getMin() {
-        return min_s.top();
+        return s.top().second;
     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(val);
+ * obj->push(value);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();
