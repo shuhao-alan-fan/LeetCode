4131c65a082c5327bd4fccf82b25cd01a3a89d8f
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-08-09 17:52:34 +0800

    Time: 0 ms (100%), Space: 19.6 MB (28.46%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -12,32 +12,43 @@ class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         ListNode* head = nullptr;
-        ListNode dummy;
-        ListNode* cur = &dummy;
-        int cnt = 1;
-        while(list1 != nullptr || list2!=nullptr){
-
-            if(list1 == nullptr){
-                cur->next = list2;
-                list2 = list2->next;
-            }
-            else if (list2 == nullptr){
+        if(list1==nullptr) return list2;
+        if(list2==nullptr) return list1;
+        if(list1->val <= list2->val){
+            head = list1;
+            list1 = list1->next;
+        }
+        else{
+            head = list2;
+            list2 = list2->next;
+        }
+        ListNode* cur = head;
+    
+        while(list1 && list2){
+            if(list1->val <= list2->val){
                 cur->next = list1;
                 list1 = list1->next;
-            }
-
-            else if(list1->val < list2->val){
-                cur->next = list1;
-                list1 = list1->next;
+                cur = cur->next;
             }
             else{
                 cur->next = list2;
                 list2 = list2->next;
+                cur = cur->next;
             }
-            
-            cur = cur->next;
-            if(cnt == 1) head = cur;
-            cnt++;
+        }
+        if(list1 == nullptr){
+            while(list2){
+                cur->next = list2;
+                list2 = list2->next;
+                cur = cur->next;
+            }
+        }
+        else if(list2 == nullptr){
+            while(list1){
+                cur->next = list1;
+                list1 = list1->next;
+                cur = cur->next;
+            }
         }
         return head;
     }
