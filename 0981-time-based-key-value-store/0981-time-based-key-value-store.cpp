2d5c0ad79e6f08c2b557a3e59a96b0109717048f
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-08-21 17:26:00 +0800

    Time: 27 ms (97.88%), Space: 136.5 MB (94.25%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,18 +1,7 @@
 class TimeMap {
 private:
     unordered_map<string, vector<pair<string,int>>> time_map;
-    int binary_search(vector<int> nums, int target)
-    {
-        int left = 0, right = nums.size();
-        if(target < nums[left]) return -1;
-        while(left < right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] > target) right = mid;
-            else left = mid + 1;
-        }
-        if(left - 1 >= 0) return nums[left - 1];
-        return -1;
-    }
+    
 public:
     
     TimeMap() {
@@ -20,22 +9,22 @@ public:
     }
     
     void set(string key, string value, int timestamp) {
-        time_map[key].push_back(make_pair(value,timestamp));
+        time_map[key].push_back({value,timestamp});
     }
     
     string get(string key, int timestamp) {
         if(time_map.find(key) == time_map.end()) return "";
-        
-        int left = 0, right = time_map[key].size();
+        auto& entries = time_map[key];
+        int left = 0, right = entries.size();
         while(left < right){
             int mid = left + (right - left) /2;
-            if(time_map[key][mid].second > timestamp) right = mid;
+            if(entries[mid].second > timestamp) right = mid;
             else left = mid + 1;
         }
         
-        if(left - 1 < 0) return "";
+        if(left == 0) return "";
 
-        return time_map[key][left-1].first;
+        return entries[left-1].first;
 
         
         
