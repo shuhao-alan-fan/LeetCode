dfed7bd85a4b89c5ac4cc0e50053d6805659598d
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-08-26 15:09:40 +0800

    Time: 71 ms (20.03%), Space: 117.4 MB (12.85%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int ans = 1;
         vector<pair<int,int>> v;
         vector<double> time;
-        
-        for(int i = 0; i<position.size(); i++){
-            v.push_back(make_pair(position[i],speed[i]));
+        for(int i = 0; i< position.size(); i++){
+            v.push_back({position[i],speed[i]});
         }
         sort(v.rbegin(),v.rend());
-        for(auto&[pos,vel]: v){
-            time.push_back(double(target - pos) / vel);
+        for(auto& [p,s] : v){
+            time.push_back(double(target - p) / s);
         }
-        double f_time = time[0];
-        for(int i = 1; i<time.size(); i++){
-            if(f_time < time[i]){
+        
+        int ans = 0;
+        double prev_time = 0.0;
+        for(int i = 0; i< time.size(); i++){
+            if(time[i] > prev_time){
                 ans++;
-                f_time = time[i];
+                prev_time = time[i];
             }
-            
         }
         return ans;
+            
+        
+        
+        
+
     }
 };
