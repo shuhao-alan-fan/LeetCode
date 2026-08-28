8860299b6425f3864ff60daed65dcc58495757f0
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-08-28 16:21:05 +0800

    Time: 52 ms (55.69%), Space: 136.6 MB (71.81%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -1,36 +1,33 @@
 class TimeMap {
-private:
-    unordered_map<string, vector<pair<string,int>>> time_map;
-    
 public:
-    
+    unordered_map<string, vector<pair<int,string>>> tmap;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-        time_map[key].push_back({value,timestamp});
+        tmap[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
-        if(time_map.find(key) == time_map.end()) return "";
-        auto& entries = time_map[key];
-        int left = 0, right = entries.size();
-        while(left < right){
-            int mid = left + (right - left) /2;
-            if(entries[mid].second > timestamp) right = mid;
-            else left = mid + 1;
+        if(tmap.find(key) == tmap.end()) return "";
+        auto& v = tmap[key];
+        int left = 0, right = v.size() - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(v[mid].first <= timestamp){
+                ans = max(mid,ans);
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
         }
-        
-        if(left == 0) return "";
+        if(ans == -1) return "";
+        return v[ans].second;
 
-        return entries[left-1].first;
-
-        
-        
     }
-
-    
 };
 
 /**
