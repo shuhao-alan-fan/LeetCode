c30f71472f3f8df243de78a3ebce2b0ffcd4b201
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-09-04 18:16:08 -0700

    Time: 0 ms (100%), Space: 23.8 MB (55.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,18 +3,17 @@ public:
     void moveZeroes(vector<int>& nums) {
         int left = 0, right = 1;
         while(right < nums.size()){
-            if(nums[left] == 0 && nums[right] != 0){
+            if(nums[left] == 0 && nums[right] == 0){
+                right++;
+                continue;
+            }
+            else if(nums[left] == 0 && nums[right] != 0){
                 swap(nums[left],nums[right]);
-                left++;
-                right++;
+               
             }
-            else if(nums[left] == 0 && nums[right] == 0){
-                right++;
-            }
-            else{
-                left++;
-                right++;
-            }
+            left++;
+            right++;
+    
         }
     }
 };
