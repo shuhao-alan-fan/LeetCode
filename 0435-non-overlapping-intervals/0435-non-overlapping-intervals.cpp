e958e440d8546b610590ece4e6307447fb15e99c
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-09-04 20:42:44 -0700

    Time: 42 ms (69.09%), Space: 94 MB (68.5%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),[](const vector<int>&a, vector<int>&b){
-            return a[1]<b[1];
+        sort(intervals.begin(),intervals.end(),[](const vector<int>&a, const vector<int>& b){
+            return a[1] < b[1];
         });
-        int prev_end = INT_MIN;
         int ans = 0;
-        for(int i = 0;i <intervals.size(); i++){
-            int cur_start = intervals[i][0];
-            if(cur_start < prev_end){
-                cout<<cur_start<<intervals[i][1]<<endl;
+        int prev_start = intervals[0][0], prev_end = intervals[0][1];
+        for(int i = 1; i<intervals.size(); i++){
+            if(intervals[i][0] < prev_end){
                 ans++;
             }
             else{
-                prev_end = intervals[i][1];
+                prev_end = max(prev_end, intervals[i][1]);
             }
         }
         return ans;
     }
 };
+
+
+// [1,2] [4,6] [6,8] [3,9] [3,4]
