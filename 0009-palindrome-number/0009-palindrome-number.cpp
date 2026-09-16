19585fc1a3548d0f22a1247d32c055e51ff0925a
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-09-16 13:34:09 -0400

    Time: 3 ms (43.88%), Space: 8.7 MB (36.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        //naive
-        // string word = to_string(x);
-        // int left = 0, right = word.size() - 1;
-        // while(left <= right){
-        //     if(word[left] != word[right]) return false;
-        //     left++;
-        //     right--;
-        // }
-        // return true;
-        //without conversion
-        if(x < 0) return false;
-        long reverse = 0;
-        int x_copy = x;
+        if(x<0) return false;
+        long long x_copy = (long long)x;
+        long long temp = 0;
         while(x > 0){
-            reverse = reverse * 10 + x%10;
-            x /= 10;
+            int i = x % 10;
+            x/=10;
+            temp *= 10;
+            temp += i;
+            
         }
-        return reverse == x_copy;
-
+        return temp == x_copy;
     }
 };
