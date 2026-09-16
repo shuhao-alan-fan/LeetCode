a026d11bea11a4e6ad690f542cdb8912a646ff66
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-09-16 11:25:34 -0400

    Time: 903 ms (6.81%), Space: 171.4 MB (5.44%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int> ms1, ms2;
-        int len = s1.size();
-        if(len > s2.size()) return false;
-        for(char c : s1){
-            ms1[c]++;
+        // unordered_map<char,int> dict;
+        int n = s1.size();
+        if(s2.size() < n) return false;
+        unordered_set<char> empt;
+        for(char c:s1){
+            empt.insert(c);
+            // dict[c]++;
         }
-        for(int i = 0; i<len; i++){
-            ms2[s2[i]]++;
-        }
-        if(ms1 == ms2) return true;
-        for(int i = len; i<s2.size(); i++){
-            ms2[s2[i - len]]--;
-            ms2[s2[i]]++;
-            if(ms2[s2[i-len]] == 0){
-                ms2.erase(s2[i-len]);
+        
+        int left = 0, right = left + n - 1;
+        while(left < s2.size() - n + 1){
+            while(left < s2.size() - n + 1 && !empt.count(s2[left])) left++;
+            if(permutation(s1, s2.substr(left,n))){
+                return true;
             }
-            if(ms1 == ms2) return true;
+            left++;
         }
+        
         return false;
+    }
+    bool permutation(string a, string b){
+        if(a.size() != b.size()) return false;
+        unordered_map<char,int> count(26);
+        for(int i = 0; i< a.size(); i++){
+            count[a[i]]++;
+            count[b[i]]--;
+        }
+        for(auto& [ch,co] : count){
+            if(co != 0) return false;
+        }
+        return true;
     }
 };
