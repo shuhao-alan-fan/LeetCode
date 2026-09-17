e63f038101c5d8191978d85bb8fcce52a747cd8a
Alan718 <75001847+shuhao-alan-fan@users.noreply.github.com>
2026-09-17 17:41:21 -0400

    Time: 2713 ms (5.56%), Space: 37.1 MB (5.42%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int bestClosingTime(string customers) {
+        // vector<int> count(customers.size()+1);
+        // for(int i = 0; i<count.size(); i++){
+        //     int cnt = 0;
+        //     for(int j = 0; j<customers.size(); j++){ //closes at ith hour & customer came at jth hour
+        //         if((j <  i && customers[j] == 'N') || (j >= i && customers[j] == 'Y')){
+        //             cnt++;
+        //         }
+        //     }
+        //     count[i] = cnt;
+        // }
+        vector<int> dp(customers.size() + 1);
+        int nos = 0;
+        for(char c: customers){
+            if(c == 'N'){
+                nos++;
+            }
+        }
+        cout<<nos<<endl;
+        dp[customers.size()] = nos;
+        for(int i = customers.size() - 1; i>=0; i--){
+            if(customers[i] == 'Y') dp[i] = dp[i+1] + 1;
+            else dp[i] = dp[i+1] - 1;
+            cout<<dp[i]<<i<<endl;
+        }
+        vector<pair<int,int>> ans;
+        for(int i = 0; i<customers.size()+ 1; i++){
+            
+            ans.push_back({dp[i],i});
+        }
+        sort(ans.begin(),ans.end());
+        return ans[0].second;
+    }
+};
